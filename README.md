11e3bcb2179e46649c513b25390bac2da8096703
aoirint <aoirint@gmail.com>
2025-04-19 00:45:12 +0900

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Stable Diffusion WebUI Batch Generation Script
---
+++ stable_diffusion_webui_batch_script/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from stable-diffusion-webui-batch-script!")
+
+
+if __name__ == "__main__":
+    main()
